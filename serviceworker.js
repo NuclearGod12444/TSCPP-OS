cab3f590e818a3554ac13b1b82b45989cd1d70b5
TheSCP_Guy <conreese10@gmail.com>
2026-05-07 20:56:16 -0400

    Add files via upload

---
+++ serviceworker.js
@@ -0,0 +1,23 @@
+const cacheName = 'TSCPPDESKTOP-v1';
+const staticAssets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './serviceworker.js',
+  './tscpp.png',
+];
+
+
+self.addEventListener('install', async (event) => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(staticAssets);
+});
+
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
