00dd978aba80ea468027304ea11d8dfe796e9d00
TheSCP_Guy <conreese10@gmail.com>
2026-07-09 22:39:35 -0400

    Add files via upload

--- serviceworker.js
+++ serviceworker.js
@@ -7,17 +7,26 @@ const staticAssets = [
   './tscpp.png',
 ];
 
-
-self.addEventListener('install', async (event) => {
-  const cache = await caches.open(cacheName);
-  await cache.addAll(staticAssets);
+self.addEventListener('install', (event) => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    await cache.addAll(staticAssets);
+    await self.skipWaiting();
+  })());
 });
 
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.filter((key) => key !== cacheName).map((key) => caches.delete(key)));
+    await self.clients.claim();
+  })());
+});
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+  if (event.request.method !== 'GET') return;
+  event.respondWith((async () => {
+    const cached = await caches.match(event.request, { ignoreSearch: true });
+    return cached || fetch(event.request);
+  })());
 });
